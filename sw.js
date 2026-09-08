03a5b7393fcd654d5a9917834cce772ed43a688f
mhamalgporee-ctrl <mhamalgporee@gmail.com>
2026-09-08 19:26:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,39 @@
-const CACHE_NAME = 'salahuddin-v2';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'salahuddin-app-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+// تثبيت ملف الخدمة وتخزين الملفات الأساسية
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// استرجاع البيانات المجهزة عند انقطاع الإنترنت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// تحديث التخزين عند تغيير النسخة
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
